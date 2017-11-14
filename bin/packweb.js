8dc1ab78b7d7ec594a0b6dc8cb534b9d69a0e0b2
anchao <1481988258@qq.com>
2017-11-14 13:44:27 +0800

    write chunk

--- bin/packweb.js
+++ bin/packweb.js
@@ -3,13 +3,13 @@ const packweb = require('../lib/packweb');
 
 program
 .version('0.1.0')
-.option('-i, --input', 'file entry to pack')
+.option('-i, --entry', 'file entry to pack')
 .option('-o, --output', 'dir to store bundled js')
 .parse(process.argv);
 
 const options = {};
 
-options.input = program.input || program.argv[2];
+options.entry = program.entry || program.argv[2];
 options.output = program.output || program.argv[3];
 
 packweb(options);
